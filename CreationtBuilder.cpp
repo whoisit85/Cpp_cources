72734eec3c949ef1317d5fce2e4a59a6ec25eb21
DingoV85 <schepet_ivan@ukr.net>
2019-09-23 19:53:00 +0300

    trabla

--- CreationtBuilder.cpp
+++
@@ -1,8 +0,0 @@
-#include "CreationtBuilder.h"
-#include "Builder.h"
-
-
-Builder* CreationBuilder::creationBuilder(int health, bool defense, Cell* cell, Player* player)
-{
-	return new Builder(health, defense, cell, player);
-}
